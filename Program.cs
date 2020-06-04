fb0d897166dbd70eb95713bad2928f0816311b6a
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-03 21:55:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculadoraIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a data do seu nascimento e então te darei o número de quantos dias você tem de vida");
+            Console.Write("Sua data de nascimento: ");
+            DateTime nascimento=Convert.ToDateTime(Console.ReadLine());
+            DateTime agora=DateTime.Now;
+            TimeSpan DiasDeVida= agora - nascimento;
+            int dias= DiasDeVida.Days;
+            Console.Clear();
+            Console.WriteLine($"Você tem exatos {dias} dias de vida.");
+
+            
+        }
+    }
+}
